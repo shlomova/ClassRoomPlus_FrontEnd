21f2b6e2a64e6acd23cf71cb9b8e1ace335f058c
ahron <a0548421610@gmail.com>
2024-05-06 11:47:04 +0300

    login and dashboard

---
+++ src/pages/log-in/LogIn.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const LogIn = (e) => {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    const config = {
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+          }
+    }
+    const {data}= await axios.post(`http://localhost:3000/users/login`,{email, password}, config)
+    console.log(data);
+    if (data.ok) {
+      localStorage.setItem('userInfo', JSON.stringify(data))
+      navigate('/dashboard')
+      return
+    }
+    alert('It is forbidden')
+    navigate('/')
+  }
+    return (
+      <>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+        </form>
+      </>
+    );
+  }
+
+export default LogIn
