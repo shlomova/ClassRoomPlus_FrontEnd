5aae91edf58cbd529ee4ad4e6ba1d1805a4e40b7
ahron <a0548421610@gmail.com>
2024-05-23 13:54:38 +0300

    by id

--- src/App.jsx
+++ src/App.jsx
@@ -6,46 +6,62 @@ import CoursesList from './Components/courses-list/coursesList'
 import axios from 'axios'
 import AddCourse from './Components/course-item/add-course/addCourse'
 import UtilsCheckUserAndToken from './utils/utilsCheckUserAndToken'
+import Subjects from './Components/subjects/subjects'
 
 
 function App() {
     const [addCourse, setAddCourse] = useState(false)
     const [coursesArr, setCoursesArr] = useState([])
+    const [categories, setCategories] = useState([])
     const [userId, setUserId] = useState('')
     const checkUserAndToken = UtilsCheckUserAndToken()
+
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const { data } = await axios.get('http://localhost:3000/courses');
                 setCoursesArr(data.courses);
+                setCategories(data.courses.map(course => {
+                    return {
+                        _id: course._id,
+                        courseName: course.courseName
+                    }
+                }))
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         };
         fetchData();
     }, [])
-    useEffect(() =>{
+
+    useEffect(() => {
         const data = checkUserAndToken();
         if (data) {
             setUserId(`${data.user._id}`);
         }
-    })
+    }, [])
     const handleButton = () => {
         setAddCourse(!addCourse)
     }
     const handleClose = () => {
         setAddCourse(false);
     };
+
     return (
         <>
-        <div className='container'>
-            
-            <CoursesList courses={coursesArr}/>
-        </div>
-        <button id='AppButton' onClick={handleButton}> To add a new course  </button>
-          {addCourse && (
+            <div htmlFor=""> Choose a topic
+                <Subjects
+                    courses={coursesArr}
+                    setCourses={setCoursesArr}
+                    categories={categories}
+                />
+            </div>
+            <button id='AppButton' onClick={handleButton}> To add a new course  </button>
+            {addCourse && (
                 <AddCourse onClose={handleClose} userId={userId} />
             )}
+
+
         </>
     )
 
--- src/Components/course-item/courseItem.jsx
+++ src/Components/course-item/courseItem.jsx
@@ -2,32 +2,33 @@ import React from "react"
 import './courseItem.css'
 import { useNavigate } from "react-router-dom"
 
-const CourseItem = ({openDate, endDate, id, courseName, description, price, userId}) => {
+const CourseItem = ({ openDate, endDate, id, courseName, description, price, userId }) => {
   const navigate = useNavigate()
-  const handleButton =() => {
+  const handleButton = () => {
     navigate('/contentsClass', { state: { openDate, endDate, id, courseName, description, price } })
   }
- 
+
   return (
- 
+
     <div className="card ">
-        <p id="courseName">{courseName}</p>
-        <p className="pCI">{description}</p>
-        <label className="d-flex" htmlFor=""> open data:
+      <div id="courseName">{courseName}</div>
+      <div className="pCI">{description}</div>
+      <label className="d-flex" htmlFor=""> open data:
         <h2 id="opendate">{openDate}</h2>
-        </label>
-        <label className="d-flex" htmlFor=""> end date:
-        <p id="endDate"> {endDate}</p>
-        </label>
-        <label className="d-flex" htmlFor=""> price: 
-        <p className="pCI"> {price}</p>
-        </label>
-        <label htmlFor="">user id:
-        <p >{userId} </p>
-        </label>
-        <button className="button" id = {id} onClick={handleButton}> to enter the course</button>
+      </label>
+      <div className="d-flex gap-1" >
+        <div id="b">end date:</div>
+        <div id="endDate"> {endDate}</div>
+      </div>
+      <label className="d-flex" htmlFor=""> price:
+        <div className="pCI"> {price}</div>
+      </label>
+      <label htmlFor="">user id:
+        <div>{userId} </div>
+      </label>
+      <button className="button" id={id} onClick={handleButton}> to enter the course</button>
     </div>
-  
+
   )
 }
 
