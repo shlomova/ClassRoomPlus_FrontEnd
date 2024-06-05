bbbb25e2b1d85e5cb442cd279080b9d7d5b83f97
rafimahr <rafimahr3@gmail.com>
2024-06-05 17:00:10 +0300

    chore: Add Option component for chatbot interaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,42 @@
-
-import './App.css'
-import Header from "./Components/header/Header.jsx";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Home from "./pages/home/Home.jsx";
-import Dashboard from "./pages/dashboard/dashboard.jsx";
-import Login from "./pages/login/login.jsx";
-import Footer from "./Components/footer/Footer.jsx"
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home/>,
-    },
-    {
-        path: '/dashboard',
-        element: <Dashboard/>
-    },
-    {
-        path: '/login',
-        element: <Login/>
-    }
-    // {
-    //     path: '/sign-up',
-    //     element: <SignUp/>
-    // },
-    // {
-    //     path: '/about',
-    //     element: <About/>
-    // },
-    // {
-    //     path: 'courses',
-    //     element: <Coureses/>
-    // }
-]);
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/header/Header.jsx';
+import Footer from './Components/footer/Footer.jsx';
+import Chatbot from './Components/chatbot/chatbot.jsx';
+import Home from './pages/home/Home.jsx';
+import Dashboard from './pages/dashboard/dashboard.jsx';
+import Login from './pages/login/login.jsx';
+// import SignUp from "./pages/sign-up/sign-up.jsx";
+import Approvestudent from './Components/approve/approve.jsx';
+import About from './Components/header/About.jsx';
+import CouresesHeader from './Components/header/CouresesHeader.jsx';
+import Verifi from './Components/approve/Verifi.jsx';
 
 function App() {
-
     return (
-        <>
-            <Header/>
-            <RouterProvider router={router}/>
-
-                {/*<Subjects/>*/}
-
-            <Footer/>
-
-        </>
-    )
-
+        <Router>
+            <div>
+                {/* Render Chatbot component */}
+                <Chatbot />
+                {/* Header */}
+                <Header />
+                {/* Routes */}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/login" element={<Login />} />
+                    {/* <Route path="/sign-up" element={<SignUp />} /> */}
+                    <Route path="/about" element={<About />} />
+                    <Route path="/courses" element={<CouresesHeader />} />
+                    <Route path="/approve" element={<Approvestudent />} />
+                    <Route path="/verifi" element={<Verifi />} />
+                </Routes>
+                {/* Footer */}
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
---
+++ src/Components/chatbot/Question.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Option from './Option';
+
+const Question = ({ question, onOptionClick }) => {
+    return (
+      <div className="question-container">
+        <p className="question-text">{question.text}</p>
+        <div className="options-container">
+          {question.options.map(option => (
+            <Option key={option.id} option={option} onClick={onOptionClick} />
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
+  export default Question;
+  
